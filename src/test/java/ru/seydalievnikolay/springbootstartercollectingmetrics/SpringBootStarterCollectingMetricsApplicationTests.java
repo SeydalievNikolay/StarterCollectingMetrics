5f967bd7800737d8ac2d8287ef070492960eacbb
Seydalievnikolay <118976386+Seydalievnikolay@users.noreply.github.com>
2023-11-26 21:26:21 +0700

    First commit

---
+++ src/test/java/ru/seydalievnikolay/springbootstartercollectingmetrics/SpringBootStarterCollectingMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.seydalievnikolay.springbootstartercollectingmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterCollectingMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
