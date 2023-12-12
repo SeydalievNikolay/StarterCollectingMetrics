f0b5e8c282f8d161a27f9bb8c50bfa9a95132c36
Seydalievnikolay <118976386+Seydalievnikolay@users.noreply.github.com>
2023-12-12 16:10:11 +0700

    refactor(create @ComponentScan annotation)

--- src/main/java/ru/seydalievnikolay/springbootstartercollectingmetrics/config/StarterCollectingMetricsConfiguration.java
+++ src/main/java/ru/seydalievnikolay/springbootstartercollectingmetrics/config/StarterCollectingMetricsConfiguration.java
@@ -2,10 +2,12 @@ package ru.seydalievnikolay.springbootstartercollectingmetrics.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import ru.seydalievnikolay.springbootstartercollectingmetrics.service.MetricsService;
 
 @Configuration
+@ComponentScan
 @EnableConfigurationProperties(StarterCollectingMetricsProperties.class)
 public class StarterCollectingMetricsConfiguration {
     @Bean
--- src/main/java/ru/seydalievnikolay/springbootstartercollectingmetrics/service/MetricsService.java
+++ src/main/java/ru/seydalievnikolay/springbootstartercollectingmetrics/service/MetricsService.java
@@ -3,17 +3,17 @@ package ru.seydalievnikolay.springbootstartercollectingmetrics.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import ru.seydalievnikolay.springbootstartercollectingmetrics.config.StarterCollectingMetricsProperties;
 
-@Component
+@Service
 public class MetricsService {
-
+    private static final Logger LOG = LoggerFactory.getLogger(MetricsService.class);
     @Autowired
     public MetricsService(StarterCollectingMetricsProperties metricsProperties) {
     }
-
-    private static final Logger LOG = LoggerFactory.getLogger(MetricsService.class);
 
     public void logMetric(String metricName) {
         LOG.info(String.format("'logMetric' call for metric: '%s'", metricName));
@@ -22,5 +22,4 @@ public class MetricsService {
     public void logErrorMetric(String metricName) {
         LOG.info(String.format("'logErrorMetric' call for metric: '%s'", metricName));
     }
-
 }
